1f1658f2f0109f86f734735f0d3aad883ed0e703
vnctwong <vincentkywong@gmail.com>
2019-02-11 22:11:43 +0000

    s3-printWithParameters.js

---
+++ s4-printingWithCallbacks.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+};
+//by passing options thru function we can call host and path 
+//from key-value objects in another module
+
+function getAndPrintHTML(options) {
+    //var buffer = [];
+    var buffer = '';
+
+    /* Add your code here */
+    https.get(options, function (response) {
+        response.setEncoding('utf8');
+
+        response.on('data', function (data) {
+            //buffer.push(data);
+            buffer += data
+            //printing in this parameter may print every chunk
+        });
+        //as each chunk recieved, assign to variable. this data 'loops' itself
+        //print variable.
+        response.on('end', function () {
+            //console.log(buffer.join(''));  // printing here before break prints once
+            console.log(buffer);
+            console.log('Response stream complete.');
+        })
+    })
+
+}
+getAndPrintHTML(requestOptions);
