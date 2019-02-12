3c67f7f39cc9e005304c953425274dc533827623
vnctwong <vincentkywong@gmail.com>
2019-02-12 01:35:22 +0000

    print_reverse.js

---
+++ callbacks/print_reverse.js
@@ -0,0 +1,13 @@
+var getHTML = require('./htmlModule.js');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse(html) {
+    console.log(html.split('').reverse().join(''));
+
+}
+
+getHTML(requestOptions, printReverse);
